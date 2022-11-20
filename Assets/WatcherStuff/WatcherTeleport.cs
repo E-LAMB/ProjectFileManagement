74550b61eeed14c4da96aa9135931e939e93c512
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2022-11-20 13:33:49 +0000

    Pathfinding System

--- Assets/WatcherStuff/WatcherTeleport.cs
+++ Assets/WatcherStuff/WatcherTeleport.cs
@@ -9,11 +9,17 @@ public class WatcherTeleport : MonoBehaviour
 
     public Transform watcher; // This is the Watcher's position
     public Transform player; // This is the Player's position
+    public Vector3 stored_position;
 
     public float max_cooldown = 4.0f; // How long between each teleport
     public float max_distance = 5.0f; // How far each teleport can reach
 
     public float cooldown;
+
+    public int stage;
+    // 1 = taking position
+    // 2 = teleported to position
+    // 3 = fatigue afterwards
 
     // Start is called before the first frame update
     void Start()
@@ -25,12 +31,24 @@ public class WatcherTeleport : MonoBehaviour
     void Update()
     {
 
-        player.position.y = watcher.position.y;
-        
-        if (cooldown < 0)
+        if (cooldown < 0 && stage == 1)
         {
-            watcher.position = Vector3.MoveTowards(watcher.position, player.position, max_distance);
+            stored_position = player.position;
             cooldown = max_cooldown;
+            stage = 2;
+        }
+        
+        if (cooldown < 0 && stage == 2)
+        {
+            watcher.position = Vector3.MoveTowards(watcher.position, stored_position, max_distance);
+            cooldown = max_cooldown;
+            stage = 3;
+        } 
+
+        if (cooldown < 0 && stage == 3)
+        {
+            cooldown = max_cooldown;
+            stage = 1;
         } 
 
         cooldown = cooldown - Time.deltaTime;
